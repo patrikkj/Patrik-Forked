cb5ad484a617e48ca5c285007f8c865dc032e023
patrikkj <35503519+patrikkj@users.noreply.github.com>
2018-02-16 17:21:28 +0100

    Twitter

--- ovinger/src/objectstructures/TwitterAccount.java
+++ ovinger/src/objectstructures/TwitterAccount.java
@@ -1,17 +1,94 @@
 package objectstructures;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class TwitterAccount {
-	private String username;
-	private List<TwitterAccount> following;
-	private int tweetCount;
+	//Instance variables
+	private final String username;
+	private HashSet<TwitterAccount> following;
+	private List<Tweet> tweetList;
 	private int retweetCount;
-	private char[][] board;
 	
+	
+	//Constructor
 	public TwitterAccount(String username) {
+		this.username = username;
 		
+		//Initialize collections
+		following = new HashSet<TwitterAccount>();
+		tweetList = new ArrayList<Tweet>();
 	}
+	
+	
+	//Getters
+	public String getUserName() {
+		return username;
+	}
+	
+	public Tweet getTweet(int i) {
+		//Validate tweet index
+		if (i < 1  ||  i > tweetList.size())
+			throw new IndexOutOfBoundsException("Tweet index is not within list boundaries. Index: " + i);
+		
+		return tweetList.get(tweetList.size() - i);
+	}
+	
+	public int getTweetCount() {
+		return tweetList.size();
+	}
+	
+	public int getRetweetCount() {
+		return retweetCount;
+	}
+	
+	public boolean isFollowing(TwitterAccount account) {
+		//If HashSet contains account, return true, else false
+		return following.contains(account);
+	}
+	
+	public boolean isFollowedBy(TwitterAccount account) {
+		return account.isFollowing(this);
+	}
+
+	
+	//Actions
+	public void follow(TwitterAccount account) {
+		//Add account to following list
+		following.add(account);
+	}
+	
+	public void unfollow(TwitterAccount account) {
+		//Remove account from list of following accounts
+		following.remove(account);
+	}
+	
+	public void tweet(String text) {
+		//Add new tweet to tweetList
+		tweetList.add(new Tweet(this, text));
+	}
+	
+	public void retweet(Tweet tweet) {
+		//Retweet with tweet as parent
+		tweetList.add(new Tweet(this, tweet));
+		
+		//Root tweet
+		Tweet originalTweet = tweet.getOriginalTweet();
+		
+		//Increment retweet count of original tweet owner
+		if (originalTweet != null)
+			//If the tweet parent is not the original owner
+			originalTweet.getOwner().retweetCount++;
+		else 
+			//If this is the first retweet in the chain
+			tweet.getOwner().retweetCount++;
+	}
+	
+	
+	
+	//Other
 	public static void main(String[] args) {
+		
 	}
 }
