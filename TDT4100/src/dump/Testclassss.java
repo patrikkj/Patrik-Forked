293f0747c818358696cd1dc32059240887cc718e
patrikkj <35503519+patrikkj@users.noreply.github.com>
2018-03-07 22:30:01 +0100

    SokoFinished :)

--- TDT4100/src/dump/Testclassss.java
+++ TDT4100/src/dump/Testclassss.java
@@ -1,17 +1,12 @@
 package dump;
 
+import java.util.Arrays;
+
 public class Testclassss {
-	private static char char1 = 'a';
-	private static char char2 = 'a';
-	
-	private static Character char3 = new Character('a');
-	private static Character char4 = new Character('a');
 	
 	public static void main(String[] args) {
-		System.out.println(char1 == char2);
-		
-		System.out.println(char3 == char4);
+		for (char c : System.lineSeparator().toCharArray())
+			System.out.println(c);
 	}
-	
 	
 }
